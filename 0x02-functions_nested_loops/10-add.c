76ece73ce173019d42feddd8838b96f138445e36
Enochneier <enochneier@gmail.com>
2023-03-04 03:53:48 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check Holberton
+ * integer a
+ * integer b
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
