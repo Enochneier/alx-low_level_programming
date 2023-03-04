da95fa04792af3f4564a903e29fad3cd8ac227eb
Enochneier <enochneier@gmail.com>
2023-03-04 05:41:52 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,14 @@
 
 /**
  * _isupper - A function that checks for uppercase character.
+ * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
 int _isupper(int c)
 {
-	char uc = 'A';
-	int isupper = 0;
-
-	for (; uc <= 'Z'; uc++)
-	{
-		if (c == uc)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-
-	return (isupper);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else 
+		return (0);
 }
+
